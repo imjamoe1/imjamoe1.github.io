36e5ad3eccbbb16f561a5977e1a91c505ccf0ff3
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-05-12 10:08:33 +0300

    Update interface.js

--- interface.js
+++ interface.js
@@ -218,7 +218,7 @@
             info = new create(object);
             info.create();
             scroll.minus(info.render());
-            data.slice(0, viewall ? data.length : 2).forEach(this.append.bind(this));
+            data.slice(0, 7).forEach(this.append.bind(this));
             html.append(info.render());
             html.append(scroll.render());
 
