4f47f39eaaffd458aec6b0e3f611196867063341
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-05-12 10:24:38 +0300

    Update interface.js

--- interface.js
+++ interface.js
@@ -476,7 +476,7 @@
                 margin-top: 0.3em;
                 margin-bottom: 0.3em;
                 max-width: 7em;
-                max-height: 3em;
+                max-height: 2em;
                 object-fit: contain;
                 width: auto;
                 height: auto;
