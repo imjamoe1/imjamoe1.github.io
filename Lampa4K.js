f8029c1d84a14a30e9a16987f5ea0f418011be79
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-05-17 13:28:32 +0300

    Update Lampa4K.js

--- Lampa4K.js
+++ Lampa4K.js
@@ -3,50 +3,55 @@
 
     Plugin.register('kino4', {
         title: '4Kino.cc',
-        version: '1.0',
+        version: '1.0.1',
+        icon: 'https://4kino.cc/favicon.ico',
         author: '4K Films',
         description: 'Просмотр фильмов с 4kino.cc',
+        priority: 100, // Приоритет в списке
         types: ['movie', 'tv'],
         onload: function () {
-            Lampa.ApiSource.add('kino4', {
-                title: '4kino.cc',
-                handler: function (params, oncomplete, onerror) {
-                    // Загрузка списка фильмов
-                    fetch('https://4kino.cc/page/1/')
-                        .then(res => res.text())
-                        .then(html => {
-                            const parser = new DOMParser();
-                            const doc = parser.parseFromString(html, 'text/html');
-                            const items = [...doc.querySelectorAll('.shortstory')].map(el => {
-                                const link = el.querySelector('a')?.href;
-                                const title = el.querySelector('.title')?.innerText;
-                                const poster = el.querySelector('img')?.src;
-                                return { title, poster, url: link };
-                            });
-                            oncomplete(items);
-                        })
-                        .catch(onerror);
+            Lampa.ApiSource.add({
+                key: 'kino4', 
+                name: '4kino.cc',
+                menu: true, // Показывать в основном меню
+                type: ['movie', 'tv'],
+                search: true, // Включить поиск
+                handler: async (params) => {
+                    try {
+                        const html = await Lampa.Utils.fetchProxy('https://4kino.cc/page/' + (params.page || 1) + '/');
+                        const parser = new DOMParser();
+                        const doc = parser.parseFromString(html, 'text/html');
+                        
+                        return {
+                            results: Array.from(doc.querySelectorAll('.shortstory')).map(el => ({
+                                title: el.querySelector('.title')?.textContent,
+                                poster: el.querySelector('img')?.getAttribute('src'),
+                                url: el.querySelector('a')?.href
+                            })),
+                            hasMore: !!doc.querySelector('.pagination .current + a')
+                        }
+                    } catch (e) {
+                        console.error('Kino4 error:', e);
+                        return { results: [] };
+                    }
                 },
-                details: function (url, oncomplete, onerror) {
-                    fetch(url)
-                        .then(res => res.text())
-                        .then(html => {
-                            const playerUrls = [...html.matchAll(/src=["'](\/\/[a-z0-9.:\/_-]+player.*?)["']/gi)]
-                                .map(m => 'https:' + m[1]);
-
-                            const videos = playerUrls.map((link, i) => ({
-                                file: link,
-                                quality: 'auto',
-                                label: `Плеер #${i + 1}`
-                            }));
-
-                            oncomplete({
-                                title: 'Фильм',
-                                description: 'Просмотр с 4kino.cc',
-                                videos
-                            });
-                        })
-                        .catch(onerror);
+                details: async (url) => {
+                    try {
+                        const html = await Lampa.Utils.fetchProxy(url);
+                        const doc = new DOMParser().parseFromString(html, 'text/html');
+                        
+                        return {
+                            title: doc.querySelector('h1')?.textContent,
+                            description: doc.querySelector('.fdesc')?.textContent,
+                            videos: Array.from(doc.querySelectorAll('iframe[src*="player"]')).map(iframe => ({
+                                file: iframe.src.startsWith('//') ? 'https:' + iframe.src : iframe.src,
+                                quality: 'HD'
+                            }))
+                        }
+                    } catch (e) {
+                        console.error('Kino4 details error:', e);
+                        return { videos: [] };
+                    }
                 }
             });
         }
