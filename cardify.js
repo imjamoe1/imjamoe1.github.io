e6b0295494e95b867f82c0a76ce2592ea58a5618
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-05-17 14:18:24 +0300

    Create cardify.js

---
+++ cardify.js
@@ -0,0 +1,83 @@
+(function() {
+    'use strict';
+
+    Lampa.Platform.tv();
+    (function(_0x3e10cd, _0x2115ee) {
+        var _0x1e7e07 = _0x26fe,
+            _0x11e883 = _0x3e10cd();
+        while (!![]) {
+            try {
+                var _0x3e56fb = parseInt(_0x1e7e07(0x7e)) / 0x1 * (-parseInt(_0x1e7e07(0x78)) / 0x2) + -parseInt(_0x1e7e07(0x80)) / 0x3 * (parseInt(_0x1e7e07(0x69)) / 0x4) + parseInt(_0x1e7e07(0x95)) / 0x5 + parseInt(_0x1e7e07(0x7c)) / 0x6 + parseInt(_0x1e7e07(0x8c)) / 0x7 + -parseInt(_0x1e7e07(0x9b)) / 0x8 + parseInt(_0x1e7e07(0x75)) / 0x9;
+                if (_0x3e56fb === _0x2115ee) break;
+                else _0x11e883['push'](_0x11e883['shift']());
+            } catch (_0xe1f860) {
+                _0x11e883['push'](_0x11e883['shift']());
+            }
+        }
+    }(_0x289c, 0x709f3), (function() {
+        var _0x2fc151 = _0x26fe,
+            _0x2c6bed = (function() {
+                var _0x1a99ee = !![];
+                return function(_0x1614e7, _0x2d1def) {
+                    var _0x2b1b19 = _0x1a99ee ? function() {
+                        var _0xbe78eb = _0x26fe;
+                        if (_0x2d1def) {
+                            var _0x2b75e5 = _0x2d1def[_0xbe78eb(0x71)](_0x1614e7, arguments);
+                            return _0x2d1def = null, _0x2b75e5;
+                        }
+                    } : function() {};
+                    return _0x1a99ee = ![], _0x2b1b19;
+                };
+            }()),
+            _0x2a457b = (function() {
+                var _0x34d519 = !![];
+                return function(_0x2e4b71, _0x3ce720) {
+                    var _0x3b649b = _0x34d519 ? function() {
+                        if (_0x3ce720) {
+                            var _0x413a18 = _0x3ce720['apply'](_0x2e4b71, arguments);
+                            return _0x3ce720 = null, _0x413a18;
+                        }
+                    } : function() {};
+                    return _0x34d519 = ![], _0x3b649b;
+                };
+            }());
+        'use strict';
+
+        function _0x340bf6() {
+            // ... [остальной код без изменений] ...
+        }
+        if (window[_0x2fc151(0x85)]) _0x340bf6();
+        else Lampa[_0x2fc151(0x8e)][_0x2fc151(0x98)](_0x2fc151(0x6a), function(_0x273601) {
+            var _0xdef1b9 = _0x2fc151;
+            if (_0x273601[_0xdef1b9(0x8f)] == _0xdef1b9(0x76)) _0x340bf6();
+        });
+    }()));
+
+    function _0x26fe(_0x4f5109, _0x5b28f2) {
+        var _0x1a48fd = _0x289c();
+        return _0x26fe = function(_0x45fdfb, _0x2a6f6c) {
+            _0x45fdfb = _0x45fdfb - 0x66;
+            var _0x3c5690 = _0x1a48fd[_0x45fdfb];
+            return _0x3c5690;
+        }, _0x26fe(_0x4f5109, _0x5b28f2);
+    }
+
+    function _0x289c() {
+        var _0x1a1e8c = [
+            // ... [остальные элементы массива] ...
+            '<style>' +
+            '.cardify .full-start-new__body{height:80vh}' +
+            // ... [другие стили] ...' +
+            '.cardify__background.loaded:not(.dim){opacity:1}' +
+            'body:not(.menu--open) .cardify__background{background:none}' + // ← ИЗМЕНЕНИЕ ЗДЕСЬ
+            '@-webkit-keyframes animation-full-background{0%{-webkit-transform:translate3d(0,-10%,0);transform:translate3d(0,-10%,0)}100%{-webkit-transform:translate3d(0,0,0);transform:translate3d(0,0,0)}}' +
+            // ... [остальные стили] ...
+            '</style>' +
+            // ... [остальной HTML] ...
+        ];
+        _0x289c = function() {
+            return _0x1a1e8c;
+        };
+        return _0x289c();
+    }
+})();
