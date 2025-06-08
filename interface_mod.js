ec5c6b32210d3201c0bf5b18e5982c44c3601ef3
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-06-08 09:24:30 +0300

    Update interface_mod.js

--- interface_mod.js
+++ interface_mod.js
@@ -370,6 +370,16 @@
             name: Lampa.Lang.translate('interface_mod_new_plugin_name'),
             icon: '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 5C4 4.44772 4.44772 4 5 4H19C19.5523 4 20 4.44772 20 5V7C20 7.55228 19.5523 8 19 8H5C4.44772 8 4 7.55228 4 7V5Z" fill="currentColor"/><path d="M4 11C4 10.4477 4.44772 10 5 10H19C19.5523 10 20 10.4477 20 11V13C20 13.5523 19.5523 14 19 14H5C4.44772 14 4 13.5523 4 13V11Z" fill="currentColor"/><path d="M4 17C4 16.4477 4.44772 16 5 16H19C19.5523 16 20 16.4477 20 17V19C20 19.5523 19.5523 20 19 20H5C4.44772 20 4 19.5523 4 19V17Z" fill="currentColor"/></svg>'
         });
+        
+        var style = document.createElement('style');
+        style.id = 'settings_custom_styles';
+        style.textContent = `
+            .settings-folder {
+                border-radius: 20px !important;
+                overflow: hidden;
+            }
+        `;
+        document.head.appendChild(style);
         // Перемещаем пункт "Интерфейс MOD" сразу после "Интерфейс" (без зацикливания)
         function moveModSettingsFolder() {
             var $folders = $('.settings-folder');
