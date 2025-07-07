ab16a8520e8e0a1ed4dfb9e74ce6d0ca1cd94777
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-07-07 12:40:19 +0300

    Update hide_fav.js

--- hide_fav.js
+++ hide_fav.js
@@ -1,10 +1,8 @@
 (function () {
-    'use strict';
+    'use strict';	
     Lampa.Listener.follow('full', function (e) {
         if (e.type == 'complite') {
-            const render = e.object.activity.render();
-            const bookButton = render.find('.full-start__button.button--book');
-            bookButton.remove();
+            e.object.activity.render().find('.full-start__button.button--book').remove();
         }
     });
 })();
