69dcd14574d3bc73d44aa2f4a6058b3d5de6f289
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-07-17 22:39:10 +0300

    Update ratings_mod1.js

--- ratings_mod1.js
+++ ratings_mod1.js
@@ -1034,39 +1034,7 @@
 
             Lampa.Template.add(style_id, `
             <style data-id="${style_id}">
-            /* ... rest of base styles identical to pivot script ... */
-            .new-interface-info__body { width: 80%; padding-top: 1.1em; }
-            .new-interface-info__head { color: rgba(255, 255, 255, 0.6); margin-bottom: 1em; font-size: 1.3em; min-height: 1em; }
-            .new-interface-info__head span { color: #fff; }
-            .new-interface-info__title { font-size: 4em; font-weight: 600; margin-bottom: 0.3em; overflow: hidden; text-overflow: "."; display: -webkit-box; -webkit-line-clamp: 1; line-clamp: 1; -webkit-box-orient: vertical; margin-left: -0.03em; line-height: 1.3; }
-            /* .new-interface-info__details { margin-bottom: 1.6em; display: flex; align-items: center; flex-wrap: wrap; min-height: 1.9em; font-size: 1.1em; } */	    
-
-             .new-interface-info__details {
-                margin-bottom: 1em; 
-                display: block;
-                min-height: 1.9em;
-                font-size: 1.1em;
-            }
-            .line-one-details {
-                margin-bottom: 0.6em;
-                line-height: 1.5;
-            }
-            .genre-details-line {
-                margin-top: 1em;
-                line-height: 1.5;
-            }
-
-            .new-interface-info__split { margin: 0 0.5em; font-size: 0.7em; }
-            .new-interface-info__description { font-size: 1.2em; font-weight: 300; line-height: 1.5; overflow: hidden; text-overflow: "."; display: -webkit-box; -webkit-line-clamp: 4; line-clamp: 4; -webkit-box-orient: vertical; width: 70%; }
-            .new-interface .card-more__box { padding-bottom: 95%; }
-            .new-interface .full-start__background { height: 108%; top: -6em; }
-            .new-interface .card__promo { display: none; }
-            .new-interface .card.card--wide+.card-more .card-more__box { padding-bottom: 95%; }
-            .new-interface .card.card--wide .card-watched { display: none !important; }
-            body.light--version .new-interface-info__body { width: 69%; padding-top: 1.5em; }
-            body.light--version .new-interface-info { height: 25.3em; }
-            body.advanced--animation:not(.no--animation) .new-interface .card--small.card--wide.focus .card__view { animation: animation-card-focus 0.2s; }
-            body.advanced--animation:not(.no--animation) .new-interface .card--small.card--wide.animate-trigger-enter .card__view { animation: animation-trigger-enter 0.2s forwards; }    
+  
 	    
             /* --- Rating Box Styles --- */
             .new-interface .full-start__rate {
