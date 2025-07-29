ce407b1c24024304266c80cebf1aafde1da786d2
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-07-29 16:09:11 +0300

    Update series.js

--- series.js
+++ series.js
@@ -16,7 +16,7 @@
       font-weight: bold;
       padding: 0.2em 0.3em;
       border-radius: 0.5em;
-      transform: translateY(5px);
+      transform: translateY(4px);
       z-index: 2;
       pointer-events: none;
     }
