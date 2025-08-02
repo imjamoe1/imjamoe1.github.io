045397e81fc19c2b29f2d138f09d4932d1320ac4
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-08-02 12:48:53 +0300

    Update interface_mod.js

--- interface_mod.js
+++ interface_mod.js
@@ -193,7 +193,7 @@ style.innerHTML = `
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
   color: #fff;
   align-items: center;
-  transform: translate(5px, 5px);
+  transform: translate(7px, 7px);
 }
 .card__vote::before {
   height: 24px;
