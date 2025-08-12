809933edf5db103254dfa7e6a3cfe42c3042161e
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-08-12 14:23:09 +0300

    Update card_ratings1.js

--- card_ratings1.js
+++ card_ratings1.js
@@ -236,8 +236,8 @@
         if (type === 'kp') {
             iconEl.innerHTML = KP_ICON_SVG;
             iconEl.style.cssText = `
-                width: 1em;
-                height: 1em;
+                width: 0.9em;
+                height: 0.9em;
                 display: flex;
                 align-items: center;
                 justify-content: center;
