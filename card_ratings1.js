f7896b5c23593e0e1a10cf99f00d93866c0be714
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-08-14 00:53:05 +0300

    Update card_ratings1.js

--- card_ratings1.js
+++ card_ratings1.js
@@ -254,7 +254,6 @@
                 justify-content: center;
                 position: relative;
                 transform: translateY(0.5px);
-                margin-top: 1px;
             `;
             ratingEl.style.bottom = '4px';
             ratingEl.style.right = '4px';
