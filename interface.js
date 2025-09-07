61ea0e3ff47ca31b4014d15d096782a6a3a8116d
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-09-07 10:05:25 +0300

    Update interface.js

--- interface.js
+++ interface.js
@@ -1240,7 +1240,7 @@ document.querySelectorAll('.new-interface-info__description').forEach(el => {
                 font-size: 1.5em;
                 font-weight: 700;
                 word-spacing: -5px;
-                text-shadow: 3px 3px 4px #C0C0C0;
+                text-shadow: 1px 1px 2px #C0C0C0;
             }
             .new-interface-info__split { margin: 0 0.5em; font-size: 0.7em; }
             .new-interface .card-more__box { padding-bottom: 95%; }
