9a6a70ad367bcb36b49def77e7e669681abda5a3
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-10-17 22:10:11 +0300

    Update ratings.js

--- ratings.js
+++ ratings.js
@@ -289,9 +289,9 @@
         "    width: 80px !important;" +
         "}" +
         ".rate--bylampa_full .source--name {" +
-        "    width: 2.3em !important;" +
-        "    height: 2.3em !important;" +
-        "    transform: scale(1.1) !important;" +
+        "    width: 2.5em !important;" +
+        "    height: 2.5em !important;" +
+        "    transform: scale(1.2) !important;" +
         "}" +
         ".rate--green  { color: #4caf50; }" +
         ".rate--lime   { color: #cddc39; }" +
@@ -2426,6 +2426,7 @@ Lampa.Listener.follow('full', function(e) {
 
     if (!window.maxsmRatingsPlugin) startPlugin();
 })();
+
 
 
 
