350c3db936e29ad69b74031bbc5e2b2172a2ef3b
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-10-26 21:51:08 +0200

    Update card_ratings1.js

--- card_ratings1.js
+++ card_ratings1.js
@@ -400,14 +400,7 @@ function createCountryFlagElement(flagUrl) {
         pointer-events: none;
         user-select: none;
         object-fit: cover;
-        border: 2px solid rgba(255,255,255,0.5);
         background: rgba(0,0,0,0.5);
-        box-shadow: 
-            inset 0 1px 0 rgba(255, 255, 255, 0.2), /* Верхний свет */
-            inset 0 -1px 0 rgba(0, 0, 0, 0.5), /* Нижняя тень */
-            0 0 0 2px rgba(0, 0, 0, 0.5), /* Внутренняя тень */
-            0 4px 15px rgba(0, 0, 0, 0.5), /* Основная тень */
-            0 8px 25px rgba(0, 0, 0, 0.4); /* Размытая тень для глубины */
         transform-origin: center center;
         transition: transform 0.1s linear;
     `;
