acac14548b810c97547f44afc85bc7f4b2a44602
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-11-02 06:32:33 +0200

    Update ratings.js

--- ratings.js
+++ ratings.js
@@ -379,7 +379,7 @@
     var KP_API_KEYS   = (window.RATINGS_PLUGIN_TOKENS && window.RATINGS_PLUGIN_TOKENS.KP_API_KEYS)   || ['ae8d6b29-b4ea-4f44-ad64-e99cb243289a', '5421e2f6-e0ed-4c08-aee9-492334f88937', '34abd082-4543-44a2-84fb-2169f49ce93e']; // api ключи массивом
     var PROXY_TIMEOUT = 5000; // Таймаут прокси
     var JACRED_PROTOCOL = 'http://'; // Протокол JacRed
-    var JACRED_URL = Lampa.Storage.get('') || 'www.jacred.xyz'; // Адрес JacRed для получения информации о карточках без протокола (jacred.xyz)
+    var JACRED_URL = Lampa.Storage.get('www.jacred.xyz'); // Адрес JacRed для получения информации о карточках без протокола (jacred.xyz)
     var JACRED_API_KEY = Lampa.Storage.get(''); // api ключ JacRed
     //var JACRED_URL = 'parser.ruzha.ru';
     //var JACRED_API_KEY = 'BCqr1JX01ISh';
@@ -2425,6 +2425,7 @@ Lampa.Listener.follow('full', function(e) {
 
     if (!window.maxsmRatingsPlugin) startPlugin();
 })();
+
 
 
 
