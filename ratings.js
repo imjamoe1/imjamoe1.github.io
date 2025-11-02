f82906a8366c1ea06dc10a6238eddfe13b5e8d4f
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-11-02 06:44:39 +0200

    Update ratings.js

--- ratings.js
+++ ratings.js
@@ -381,7 +381,7 @@
     var JACRED_PROTOCOL = 'https://'; // Протокол JacRed
     //var JACRED_URL = Lampa.Storage.get('jackett_url'); // Адрес JacRed для получения информации о карточках без протокола (jacred.xyz)
     //var JACRED_API_KEY = Lampa.Storage.get('jackett_key'); // api ключ JacRed
-    var JACRED_URL = 'jacred.xyz';
+    var JACRED_URL = 'www.jacred.xyz';
     var JACRED_API_KEY = '';
     //var JACRED_URL = 'parser.ruzha.ru';
     //var JACRED_API_KEY = 'BCqr1JX01ISh';
@@ -2427,6 +2427,7 @@ Lampa.Listener.follow('full', function(e) {
 
     if (!window.maxsmRatingsPlugin) startPlugin();
 })();
+
 
 
 
