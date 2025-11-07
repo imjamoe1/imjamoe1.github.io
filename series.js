334821d833a4d3da9b99eeb32ef125ed9508ef1c
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-11-07 23:19:27 +0200

    Update series.js

--- series.js
+++ series.js
@@ -47,7 +47,7 @@
     }
     .card__series-ongoing span {
       display: inline-block;
-      transform: translateY(-1px);
+      transform: translateY(-0.5px);
       vertical-align: middle;
     }
     .card__series-ongoing-svg {
@@ -225,6 +225,7 @@
     document.addEventListener('lampaReady', init);
   }
 })();
+
 
 
 
