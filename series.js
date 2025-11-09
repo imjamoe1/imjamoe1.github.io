0394119663c6b799daffde115022fa9d971f9913
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-11-09 20:45:55 +0200

    Update series.js

--- series.js
+++ series.js
@@ -47,7 +47,7 @@
     }
     .card__series-ongoing span {
       display: inline-block;
-      transform: translateY(-1.2px);
+      transform: translateY(-1.4px);
       vertical-align: middle;
     }
     .card__series-ongoing-svg {
@@ -225,6 +225,7 @@
     document.addEventListener('lampaReady', init);
   }
 })();
+
 
 
 
