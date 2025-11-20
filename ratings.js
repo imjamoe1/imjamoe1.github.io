0ad8b94019294303759564db4d3513588bff1128
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-11-20 23:30:11 +0200

    Update ratings.js

--- ratings.js
+++ ratings.js
@@ -342,7 +342,7 @@
         ".info__rate .rate--icon svg {" +
         "    width: 100% !important;" +
         "    height: 100% !important;" +
-        "    transform: translateY(1px) translateX(-2px) scaleY(1.2) !important;" +
+        "    transform: translateY(1px) translateX(-2.5px) scaleY(1.2) !important;" +
         "}" +
         "</style>";
     
@@ -2483,6 +2483,7 @@ Lampa.Listener.follow('full', function(e) {
 
     if (!window.maxsmRatingsPlugin) startPlugin();
 })();
+
 
 
 
