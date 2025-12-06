badbe6461c364c767317f061c9281a96c0d26859
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-12-06 22:26:59 +0200

    Update sort_menu1.js

--- sort_menu1.js
+++ sort_menu1.js
@@ -86,7 +86,7 @@
                 // Проверяем, не существует ли уже кнопка
                 if ($('#EXTENSIONS').length) return;
                 
-                var icon_server_extensions = '<svg height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="21" height="21" rx="2" fill="currentColor"></rect><mask id="path-2-inside-1_154:24" fill="currentColor"><rect x="2" y="27" width="17" height="17" rx="2"></rect></mask><rect x="2" y="26" width="17" height="17" rx="2" stroke="currentColor" stroke-width="2"></rect><rect x="27" y="2" width="17" height="17" rx="2" fill="currentColor"></rect><rect x="27" y="34" width="17" height="3" fill="currentColor"></rect><rect x="34" y="44" width="17" height="3" transform="rotate(-90 34 44)" fill="currentColor"></rect></svg>';
+                var icon_server_extensions = '<svg height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="21" height="21" rx="2" fill="currentColor"></rect><mask id="path-2-inside-1_154:24" fill="currentColor"><rect x="2" y="27" width="17" height="17" rx="2"></rect></mask><rect x="2" y="25" width="17" height="17" rx="2" stroke="currentColor" stroke-width="2"></rect><rect x="27" y="2" width="17" height="17" rx="2" fill="currentColor"></rect><rect x="27" y="34" width="17" height="3" fill="currentColor"></rect><rect x="34" y="44" width="17" height="3" transform="rotate(-90 34 44)" fill="currentColor"></rect></svg>';
                 
                 var extensionsBUTT = '<div id="EXTENSIONS" class="head__action selector">' + icon_server_extensions + '</div>';
                 
