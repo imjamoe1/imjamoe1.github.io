56cca69028d27e0f8de7a88414b7153e053318c4
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-12-25 12:42:01 +0200

    Update sort_menu1.js

--- sort_menu1.js
+++ sort_menu1.js
@@ -388,7 +388,7 @@
                     titleKey = 'head_action_feed';
                 } else if (mainClass.includes('open--premium')) {    
                     titleKey = 'head_action_premium';
-                } else if (mainClass.includes('notice-screen')) {    
+                } else if (mainClass.includes('notice-unread')) {    
                     titleKey = 'head_action_noticescreen';     
                 } else if (mainClass.includes('notice--icon')) {    
                     titleKey = 'head_action_notice';    
