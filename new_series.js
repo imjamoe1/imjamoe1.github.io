5ffe69746c60a4621c2bb9a1b11892e8f030d6d0
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-12-26 23:53:52 +0200

    Update new_series.js

--- new_series.js
+++ new_series.js
@@ -49,7 +49,7 @@
         '.card__series:empty { display: none; }' +
         '.card__language {' +
             'position: absolute;' +
-            'top: 5.7em;' +
+            'top: 4.7em;' +
             'left: 0em;' +
             'font-size: 0.8em;' +
             'font-weight: 700;' +
