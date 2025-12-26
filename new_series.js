a61e80a75c2d692d03e9daad54b30ee38a7af031
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-12-26 21:33:30 +0200

    Update new_series.js

--- new_series.js
+++ new_series.js
@@ -50,8 +50,8 @@
         '.card__language {' +
             'position: absolute;' +
             'top: 3.2em;' +
-            'left: 2em;' +
-            'font-size: 0.8em;' +
+            'left: 2.2em;' +
+            'font-size: 0.6em;' +
             'font-weight: 700;' +
             'color: #fff;' +
             'background: rgba(0, 0, 0, 0.7);' +
