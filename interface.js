7325a5f3009930647d891b8f36edd01e1e1cd9cf
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2025-12-30 00:09:58 +0200

    Update interface.js

--- interface.js
+++ interface.js
@@ -1089,9 +1089,9 @@ function create() {
 
         Lampa.InteractionMain = function (object) { 
             var use = new_interface;
-            if (!(object.source == 'tmdb' || object.source == 'cub')) use = old_interface;
-            //var supportedSources = ['tmdb', 'cub', 'filmix', 'rezka', 'kp', 'online', 'hdvb', 'ashdi'];
-            //if (supportedSources.indexOf(object.source) === -1) use = old_interface; 
+            //if (!(object.source == 'tmdb' || object.source == 'cub')) use = old_interface;
+            var supportedSources = ['tmdb', 'cub', 'filmix'];
+            if (supportedSources.indexOf(object.source) === -1) use = old_interface; 
             if (window.innerWidth < 767) use = old_interface; 
             if (!Lampa.Account.hasPremium()) use = old_interface; 
             if (Lampa.Manifest.app_digital < 153) use = old_interface; 
