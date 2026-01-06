29221ee00f1823cb8a95255b0e6ea2c4f3cf4d0a
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-01-06 13:02:52 +0200

    Update card_ratings1.js

--- card_ratings1.js
+++ card_ratings1.js
@@ -2,7 +2,7 @@
   'use strict';
 
     // Настройки плагина
-    const API_KEY = '14342b35-714b-449d-bf10-30d0d9ac22e6';
+    const API_KEY = 'cf4d8e72-0ef2-47b7-a5fd-08e7ad3a2939';
     const TMDB_API_KEY = '4ef0d7355d9ffb5151e987764708ce96';
     const TMDB_API_URL = 'https://api.themoviedb.org/3/';
     const KP_RATING_URL = 'https://rating.kinopoisk.ru/';
