8eb89035f135de633bc6eae9d58ed1a3c24cd64b
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-01-16 21:32:33 +0200

    Update buttons1.js

--- buttons1.js
+++ buttons1.js
@@ -460,6 +460,11 @@
 
     function applyButtonAnimation(buttons) {
         buttons.forEach(function(btn, index) {
+            // Пропускаем кнопку платформ
+            if (btn.hasClass('button--plaftorms')) {
+                btn.css('opacity', '1');  // Сразу делаем видимой
+                return;
+            }
             btn.css({
                 'opacity': '0',
                 'animation': 'button-fade-in 0.4s ease forwards',
@@ -1538,6 +1543,8 @@
                 .concat(categories.plaftorms)
                 .concat(categories.other);
 
+            var visibleButtons = [];
+
             allButtons = sortByCustomOrder(allButtons);
             allButtonsCache = allButtons;
             
@@ -1693,7 +1700,7 @@
             setTimeout(function() {
                 setupButtonNavigation(container);
             }, 100);
-        }, 200); // ЗДЕСЬ ЗАДЕРЖКА 500 МИЛЛИСЕКУНД
+        }, 100); // ЗДЕСЬ ЗАДЕРЖКА 500 МИЛЛИСЕКУНД
 
         return true;
     }
@@ -1778,7 +1785,7 @@
                             if (targetContainer.length) {
                                 setTimeout(function() {
                                     targetContainer.removeClass('buttons-loading');
-                                }, 300); // Чуть больше чем задержка в reorderButtons
+                                }, 150); // Чуть больше чем задержка в reorderButtons
                                 refreshController();
                             }
                         }
