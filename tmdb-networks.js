61ccce523027b5891326910984a026e705e38159
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-02-06 21:16:22 +0200

    Update tmdb-networks.js

--- tmdb-networks.js
+++ tmdb-networks.js
@@ -578,6 +578,7 @@ function renderNetworks() {
         $('<style>').prop('type', 'text/css').html(
             '.tmdb-networks { margin-top: -3em; } ' +
             '.network-btn { height: 4.5em; } ' +
+            '.network-btn.movie { height: 4em; } ' +
             '.network-logo { 
                 background-color: transparent;
                 position: relative; 
