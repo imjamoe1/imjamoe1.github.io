70ad5756eacb11a269937fe450c4f7d267bf788f
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-02-06 20:23:14 +0200

    Update tmdb-networks.js

--- tmdb-networks.js
+++ tmdb-networks.js
@@ -577,15 +577,16 @@ function renderNetworks() {
 
         $('<style>').prop('type', 'text/css').html(
             '.tmdb-networks { margin-top: -3em; } ' +
-            '.network-btn { height: 4.5em; } ' + // Еще больше
-            '.network-logo { background-color: #fff; position: relative; padding: 0.4em; } ' +
+            '.network-btn { height: 2.94em; } ' +
+            '.network-btn.movie { height: 4em; } ' +
+            '.network-logo { background-color: #fff; position: relative; } ' +
+            '.network-logo.movie { background: none; padding: 0; } ' +
             '.network-logo .overlay { ' +
                 'position: absolute; top: 0; left: 0; right: 0; bottom: 0; ' +
                 'background: rgba(0, 0, 0, 0); ' +
             '} ' +
-            '.network-logo img { border-radius: 0.8em; height: 100%; width: auto; } ' + // width: auto для пропорций
-            '.network-logo.full-start__button { height: 5em; padding: 0.5em; }' + // Особый размер для кнопки в начале
-            '.network-logo.full-start__button .overlay { border-radius: 1.5em; }' +
+            '.network-logo img { border-radius: 0.6em; height: 100%; } ' +
+            '.network-logo.full-start__button .overlay, .network-logo.full-start__button.movie * { border-radius: 1em }' + 
             '.network-logo.focus .overlay { background: rgba(0, 0, 0, 0.3); } ' +
             '.network-logo.focus { box-shadow: 0 0 0 0.3em rgb(255, 255, 255); }' +
         '.platforms-static { ' +
