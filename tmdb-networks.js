140f324406e5cb88bbe29894a40308eae24d4050
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-02-06 20:40:19 +0200

    Update tmdb-networks.js

--- tmdb-networks.js
+++ tmdb-networks.js
@@ -578,7 +578,9 @@ function renderNetworks() {
         $('<style>').prop('type', 'text/css').html(
             '.tmdb-networks { margin-top: -3em; } ' +
             '.network-btn { height: 4em; } ' +
+            '.network-btn.movie { height: 4em; } ' +
             '.network-logo { background-color: #fff; position: relative; } ' +
+            '.network-logo.movie { background: none; padding: 0; } ' +
             '.network-logo .overlay { ' +
                 'position: absolute; top: 0; left: 0; right: 0; bottom: 0; ' +
                 'background: rgba(0, 0, 0, 0); ' +
