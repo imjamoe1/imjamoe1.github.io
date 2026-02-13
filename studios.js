e254b1b0e1ba8fd5ff5b9e724c2519238634beea
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-02-13 11:16:05 +0200

    Update studios.js

--- studios.js
+++ studios.js
@@ -353,7 +353,7 @@
                     
                     .studios-static__text {
                         font-size: 1.2em !important;
-                        font-weight: 600 !important;
+                        font-weight: 400 !important;
                         color: rgba(255, 255, 255, 0.9) !important;
                     }
                     
@@ -406,3 +406,4 @@
         });
     }
 })();
+
