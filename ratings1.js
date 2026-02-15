fb539234000d3126abeae9a5f857c37fdc6c2a29
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-02-15 12:56:44 +0200

    Adjust top position of quality badge in CSS

--- ratings1.js
+++ ratings1.js
@@ -3501,7 +3501,7 @@ var qualityTagsStyle = "<style id='maxsm-quality-tags'>" +
     ".quality-tags-wrapper .quality-badge {" +
     "    height: 1.3em !important;" +
     "    position: relative !important;" +
-    "    top: 0.1em !important;" + 
+    "    top: 0.2em !important;" + 
     "}" +
     ".quality-tags-wrapper .quality-separator {" +
     "    margin: 0 0.3em !important;" +
