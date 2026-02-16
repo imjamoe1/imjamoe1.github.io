825bf1fcbdacfd698a62d287484880deda62be5f
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-02-16 20:22:37 +0200

    Update ratings1.js

--- ratings1.js
+++ ratings1.js
@@ -3290,7 +3290,7 @@ Lampa.Listener.follow('full', function(e) {
             margin-left: 0.6em;
             padding: 0.3em 0.8em !important;
             background-color: rgba(0, 0, 0, 0.7) !important;
-            border: 1.2px solid #fff !important;
+            border: 0.8px solid #fff !important;
             border-radius: 0.7em !important;
             box-shadow: 0 2px 4px rgba(0,0,0,0.1) !important;
             opacity: 0;
