35c54ab69db147a31ba94269ad0a86fbd4e97597
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-02-26 17:49:38 +0200

    Update left_menu.js

--- left_menu.js
+++ left_menu.js
@@ -22,7 +22,7 @@
                 <style id="menu_always_style">
                     /* Режим "Всегда показывать меню" */
                     body.menu--always .wrap__left {
-                        width: 5.4% !important;
+                        width: 5% !important;
                         margin-left: 0 !important;
                         transform: translate3d(0, 0, 0) !important;
                         visibility: visible !important;
@@ -31,7 +31,7 @@
 
                     body.menu--always .wrap__content {
                         transform: translate3d(0, 0, 0) !important;
-                        margin-left: -6% !important;
+                        margin-left: -6.5% !important;
                         padding-left: 6% !important;
                     }
 
