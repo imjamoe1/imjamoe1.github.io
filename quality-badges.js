00ff110833fe7bdf290907ae945dac0db7f4d21b
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-02 10:27:18 +0200

    Update quality-badges.js

--- quality-badges.js
+++ quality-badges.js
@@ -327,7 +327,7 @@
         /* Бейджи качества */
         .quality-badges {
             position: relative;
-            bottom: 1em;
+            bottom: 3em;
             display: inline-flex;
             align-items: center;
             gap: 0.4em;
