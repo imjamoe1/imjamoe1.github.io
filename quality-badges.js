a025df4c2bb363b8f8a5e74f7a7cff5e8fa667f1
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-02 09:41:18 +0200

    Update quality-badges.js

--- quality-badges.js
+++ quality-badges.js
@@ -318,7 +318,7 @@
             badgesContainer.addClass('show');
             
             // Добавляем отступ сверху для .full-start-new__details
-            targetContainer.css('margin-top', '3em');
+            targetContainer.css('margin-top', '6em');
         }
     }
 
