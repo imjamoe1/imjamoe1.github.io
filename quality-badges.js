a0a7da00ccbb663a5bb527c86bd64792edd8169c
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-02 11:55:30 +0200

    Update quality-badges.js

--- quality-badges.js
+++ quality-badges.js
@@ -369,7 +369,7 @@
         .full-start-new__details,
         .full-start__details {
             position: relative;
-            top: 3em !important;
+            //top: 3em !important;
             //right: 8em !important;
         }
         </style>`;
