e4cfb0d6760b5277922b49b7b9543187208d76e7
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-04 18:03:03 +0200

    Update left_menu1.js

--- left_menu1.js
+++ left_menu1.js
@@ -220,33 +220,6 @@
                 }
             });
 
-            Lampa.Listener.follow('router', (e) => {
-                if (e.to === 'main' || e.to === 'category') {
-                    setTimeout(() => {
-                        $('body').removeClass('hide-compact');
-                        recalculateSizes();
-                    }, 100);
-                } else if (e.to && e.to.includes('sisi')) {
-                    setTimeout(() => {
-                        $('body').addClass('hide-compact');
-                        recalculateSizes();
-                    }, 50);
-                } else {
-                    setTimeout(applyMenuAlways, 100);
-                }
-            });
-
-            new MutationObserver((mutations) => {
-                mutations.forEach(m => {
-                    m.addedNodes.forEach(node => {
-                        if (node.nodeType === 1 && (node.classList?.contains('explorer') || node.querySelector?.('.explorer'))) {
-                            $('body').addClass('hide-compact');
-                            recalculateSizes();
-                        }
-                    });
-                });
-            }).observe(document.body, { childList: true, subtree: true });
-
             Lampa.Storage.listener.follow('change', (e) => {
                 if (e.name === 'menu_always') {
                     applyMenuAlways();
