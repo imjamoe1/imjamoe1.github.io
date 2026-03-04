b8c7525a1afbc6adb2eafd2b75c635a5c3e5be7e
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-04 02:11:10 +0200

    Update left_menu.js

--- left_menu.js
+++ left_menu.js
@@ -74,7 +74,7 @@
                         width: 6%;
                         min-width: 70px;
                         max-width: 100px;
-                        margin-left: -0.5%;
+                        margin-left: -1%;
                         transform: translate3d(0, 0, 0);
                         visibility: visible !important;
                         position: relative;
@@ -84,9 +84,9 @@
 
                     body.menu--always .wrap__content {
                         transform: translate3d(0, 0, 0);
-                        width: calc(100% - 5%);
+                        width: calc(100% - 6%);
                         flex: 1;
-                        margin-left: -1%;
+                        margin-left: -2%;
                         padding-left: 0;
                         transition: width 0.2s, transform 0.2s;
                     }
