74a6666d28b1d7ac8e2b58dfd96756e44433215e
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-08 02:42:14 +0200

    Update left_menu1.js

--- left_menu1.js
+++ left_menu1.js
@@ -63,8 +63,8 @@
                         transform: translate3d(0, 0, 0);
                         width: calc(100% - 6%);
                         flex: 1;
-                        margin-left: 4%;
-                        padding-left: 0;
+                        margin-left: -6%;
+                        padding-left: 6%;
                         transition: width 0.2s, transform 0.2s;
                     }
 
