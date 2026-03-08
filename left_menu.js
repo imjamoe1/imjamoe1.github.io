adf93ed47b13597c3038f99169efc48a50d060dc
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-08 13:27:35 +0200

    Update left_menu.js

--- left_menu.js
+++ left_menu.js
@@ -140,8 +140,8 @@
 
                     /* Когда меню открыто */
                     body.menu--always.menu--open .wrap__left {
-                        width: 17.5em;
-                        min-width: 17.5em;
+                        width: 15em;
+                        min-width: 15em;
                         margin-left: -15em;
                         //padding-right: 2.5em;
                         transform: translate3d(15em, 0, 0);
