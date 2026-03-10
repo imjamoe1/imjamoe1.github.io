54eb7df0684fd066ddc7f89b36367d9d63c07c0b
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-10 11:43:34 +0200

    Update left_menu1.js

--- left_menu1.js
+++ left_menu1.js
@@ -65,7 +65,7 @@
 
                     body.menu--always .wrap__content {
                         transform: translate3d(0, 0, 0) !important;
-                        margin-left: 0 !important;
+                        margin-left: -1em !important;
                         padding-left: 1em !important;
                         //width: 100% !important;
                         //box-sizing: border-box !important;
