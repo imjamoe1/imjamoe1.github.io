b462bbe0c75beb8599d1449ac2123cb710f3ad15
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-03-13 12:48:06 +0200

    Update left_menu.js

--- left_menu.js
+++ left_menu.js
@@ -44,7 +44,7 @@
 
                     body.menu--always:not(.menu--open) .menu__list .menu__item:hover,
                     body.menu--always:not(.menu--open) .menu__list .menu__item:focus {
-                        padding-left: -0.5em !important;
+                        margin-right: -1em !important;
                         width: 60% !important;
                     }
 
