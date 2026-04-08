36af42f5b00326e078159d0415f755092d0b6b15
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-04-08 07:35:38 +0300

    Update num.js

--- num.js
+++ num.js
@@ -7,7 +7,7 @@
     var BASE_URL = (function() {
     var scriptUrl = (document.currentScript && document.currentScript.src) || '';
     var params = new URLSearchParams(scriptUrl.split('?')[1]);
-    return params.get('base_url') || 'https://cors.bwa.workers.dev/https://num.jac-red.ru';
+    return params.get('base_url') || 'https://cors.bwa.workers.dev/https://numparser.igorek1986.ru';
     })();
     var ICON = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path fill="currentColor" d="M482.909,67.2H29.091C13.05,67.2,0,80.25,0,96.291v319.418C0,431.75,13.05,444.8,29.091,444.8h453.818c16.041,0,29.091-13.05,29.091-29.091V96.291C512,80.25,498.95,67.2,482.909,67.2z M477.091,409.891H34.909V102.109h442.182V409.891z"/></g></g><g><g><rect fill="currentColor" x="126.836" y="84.655" width="34.909" height="342.109"/></g></g><g><g><rect fill="currentColor" x="350.255" y="84.655" width="34.909" height="342.109"/></g></g><g><g><rect fill="currentColor" x="367.709" y="184.145" width="126.836" height="34.909"/></g></g><g><g><rect fill="currentColor" x="17.455" y="184.145" width="126.836" height="34.909"/></g></g><g><g><rect fill="currentColor" x="367.709" y="292.364" width="126.836" height="34.909"/></g></g><g><g><rect fill="currentColor" x="17.455" y="292.364" width="126.836" height="34.909"/></g></g></svg>';
     var DEFAULT_MIN_PROGRESS = 90;
