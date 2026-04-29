02aaea94a47406b37103f17fc898a4f69a0eb9fc
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-04-29 13:38:49 +0300

    Update tmdb-studios.js

--- tmdb-studios.js
+++ tmdb-studios.js
@@ -1327,7 +1327,7 @@
                 gap: 0.3em !important;
                 border: 0.03em solid rgba(255, 255, 255, 0.9) !important;
                 border-radius: 0.5em !important;
-                padding: 0.4em 0.4em !important;
+                padding: 0.4em 0.35em !important;
             }
             .quality-tags-container .quality-badge {
                 position: relative !important;
