7b2b3d0d2c9f2c4b6691bd1086325108d67de8fd
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-05-11 20:31:17 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -8,7 +8,7 @@
     window.plugin_interface_ready_v3 = true;
 
     // --- Конфигурация Kinopoisk ---
-    var kp_prox = 'https://cors.kp556.workers.dev/';
+    var kp_prox = 'https://cors.kp556.workers.dev:8443/';
     const KP_CONFIG = {
         api_url: kp_prox + 'https://kinopoiskapiunofficial.tech/',
         rating_url: kp_prox + 'https://rating.kinopoisk.ru/',
