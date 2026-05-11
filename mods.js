29e7281b9d1db10b3da1956edbe1b477cb155925
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-05-11 20:53:40 +0300

    Update mods.js

--- mods.js
+++ mods.js
@@ -523,7 +523,7 @@ rating_kp_imdb: function (card) {
         var search_date = card.release_date || card.first_air_date || card.last_air_date || "0000";
         var search_year = parseInt((search_date + "").slice(0, 4));
         var orig = card.original_title || card.original_name;
-        var kp_prox = "https://worker-patient-dream-26d8.bdvburik.workers.dev:8443/";
+        var kp_prox = "https://cors.kp556.workers.dev/";
         var params = {
             id: card.id,
             url: kp_prox + "https://kinopoiskapiunofficial.tech/",
