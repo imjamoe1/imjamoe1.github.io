7a037d446948bf1a86b80f90337cf15ce311af2c
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-05-24 16:41:55 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -1294,7 +1294,7 @@
                     }
                     .new-interface .full-start__background {
                         position: absolute;
-                        height: 100%;
+                        height: calc(100% + 4em);
                         width: 100%;
                         top: -4em;
                         left: 0;
