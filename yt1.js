9e61e6ea3b47b594cb6213eed8dbcc3b13874321
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-06-12 18:29:01 +0300

    Update yt1.js

--- yt1.js
+++ yt1.js
@@ -485,7 +485,7 @@
             /* Режим мини-плеера - размеры увеличенные */
             ".trailer-player__container.mini-player-mode{width:588px;height:331px;}\n" +
             /* Позиция справа - со сдвигом вниз */
-            ".trailer-player__container.mini-player-right{top:0;right:0;}\n" +
+            ".trailer-player__container.mini-player-right{top:20px;right:0;}\n" +
             /* Позиция слева - со сдвигом вниз */
             ".trailer-player__container.mini-player-left{top:120px;left:24px;}\n" +
             /* Позиция по центру */
