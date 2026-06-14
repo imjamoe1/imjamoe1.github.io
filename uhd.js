7ae1a0a6343a080c075f9940745da37a17e9c9e7
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-06-14 10:59:09 +0300

    Update uhd.js

--- uhd.js
+++ uhd.js
@@ -9,7 +9,7 @@
 
   var balansers_with_search;
   
-  var unic_id = Lampa.Storage.get('lampac_unic_id', '');
+  var unic_id = Lampa.Storage.set('lampac_unic_id', '');
   if (!unic_id) {
     unic_id = Lampa.Utils.uid(8).toLowerCase();
     Lampa.Storage.set('lampac_unic_id', 'guest');
