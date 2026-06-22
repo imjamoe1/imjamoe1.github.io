8be1ad623b67adf124721ac29a56c4592eba6afc
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-06-22 06:51:49 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -1310,9 +1310,6 @@
                     body.menu--always:not(.menu--open):not(.hide-compact) .new-interface .full-start__background {
                         left: 2em;
                     }
-                    body.menu--always .new-interface .card.card--small {
-                        width: calc(11.5em - 2.5em) !important;
-                    }
                     .card__quality {
                         font-weight: bold !important;
                     }
