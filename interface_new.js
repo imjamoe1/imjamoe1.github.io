62d4d665230f140d34f8a050ed45007616e822a5
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-06-22 08:07:17 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -943,7 +943,7 @@
                     .new-interface-info__head.visible, .new-interface-info__ratings.visible, .new-interface-info__details.visible{ opacity: 1; }
                     .new-interface .card.card--wide {
                         width: 18.3em;
-                        margin-top: -1em !important;
+                        margin-top: -1.5em !important;
                     }
                     .items-line__title .full-person__photo {
                         width: 1.8em !important;
