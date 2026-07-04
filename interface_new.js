b8497e9a9117584c181d66fd314823f27fdae1c8
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-07-04 20:09:08 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -1311,12 +1311,12 @@
                     .new-interface .full-start__background.active {
                         opacity: 1;
                     }
-                    //body.menu--always .new-interface .full-start__background {
-                        //left: 2em;
-                    //}
-                    body.menu--always .new-interface .card.card--small {
-                        width: calc(11.5em - 1.5em) !important;
+                    body.menu--always .new-interface .full-start__background {
+                        left: 2em;
                     }
+                    //body.menu--always .new-interface .card.card--small {
+                        //width: calc(11.5em - 1.5em) !important;
+                    //}
                     .full-start-new__title {
                         padding-top: 0.3em;
                     }
