8343e8178fa2520542978e943520d9c76a2a5d39
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-07-24 16:11:21 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -1310,7 +1310,7 @@
                         opacity: 1;
                     }
                     body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background {
-                        left: 2em;
+                        left: 4em;
                     }
                     body.menu--always:not(.hide-compact):not(.menu--open) .items-line__more {
                         position: relative !important;
