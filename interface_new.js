64abf5fecfd6d34058e78ab07f0db4061d1e873a
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-08-09 02:21:46 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -1310,7 +1310,7 @@
                         opacity: 1;
                     }
                     body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background-wrapper {
-                        width: calc(100% + 2em) !important;
+                        width: calc(100% + 4em) !important;
                     }
                     /* body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background {
                         left: 2em !important;
