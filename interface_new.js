f067b11e796911f096b956d1cfbb33a1c6449ff5
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-08-09 02:02:57 +0300

    Update interface_new.js

--- interface_new.js
+++ interface_new.js
@@ -1312,9 +1312,9 @@
                     body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background-wrapper {
                         width: 100%;
                     }
-                    body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background {
+                    /* body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background {
                         left: -1em !important;
-                    }
+                    } */
                     body.menu--always:not(.hide-compact):not(.menu--open) .items-line__more {
                         position: relative !important;
                         right: -5.5em !important;
