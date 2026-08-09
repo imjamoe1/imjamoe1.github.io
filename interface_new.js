c6fd1197f124e06551be8ae8e958abd88967c354
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-08-09 03:20:25 +0300

    Comment out background wrapper CSS rule
    Comment out CSS rule for full-start__background-wrapper.

--- interface_new.js
+++ interface_new.js
@@ -1309,9 +1309,9 @@
                     .new-interface .full-start__background.active {
                         opacity: 1;
                     }
-                    body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background-wrapper {
+                    /* body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background-wrapper {
                         width: calc(100% + 2em) !important;
-                    }
+                    } */
                     body.menu--always:not(.hide-compact):not(.menu--open) .full-start__background {
                         left: 2em !important;
                         width: calc(100% + 2em) !important;
