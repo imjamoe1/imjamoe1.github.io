196d1b4558e50bbc72959adb2caa20e1ebecc5cc
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-09-06 09:45:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function fetchJSFiles() {
       const url = `https://${GITHUB_USER}.github.io/${fileName}`;
       
       const container = document.createElement("div");
-      container.className = "bg-green-100 p-4 rounded shadow flex justify-between items-center";
+      container.className = "bg-green-150 p-4 rounded shadow flex justify-between items-center";
 
       const link = document.createElement("a");
       link.href = url;
