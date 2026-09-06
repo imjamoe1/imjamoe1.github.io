24ecb3ecffd724965dad4219dcdcb57e79dd068a
imjamoe1 <100382683+imjamoe1@users.noreply.github.com>
2026-09-06 10:28:42 +0300

    Refactor fetchJSFiles function and UI elements

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ async function fetchJSFiles() {
 
       const button = document.createElement("button");
       button.textContent = "📋 Copy";
-      button.className = "bg-gray-100 hover:bg-gray-500 text-black font-bold text-sm px-3 py-1 rounded";
+      button.className = "bg-gray-100 hover:bg-gray-400 text-black font-bold text-sm px-3 py-1 rounded";
       button.onclick = () => {
         navigator.clipboard.writeText(url);
         button.textContent = "✅ Copied!";
